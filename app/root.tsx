761f730025ccc25d6b40059f58f976be77b965a5
Jon Bilous <jon@jonbilous.com>
2023-07-19 10:04:02 -0400

    add sass-ui

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import type { DataFunctionArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import brand from "brand";
 import EmotionDocumentWrapper from "./chakra/components/EmotionDocumentWrapper";
 import { getUserProfile } from "./utils/auth/profile.server";
+
+import { SaasProvider } from "@saas-ui/react";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -21,9 +22,9 @@ export const loader = async (ctx: DataFunctionArgs) => {
 export default function App() {
   return (
     <EmotionDocumentWrapper>
-      <ChakraProvider>
+      <SaasProvider>
         <Outlet />
-      </ChakraProvider>
+      </SaasProvider>
     </EmotionDocumentWrapper>
   );
 }
